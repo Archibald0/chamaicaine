4d5a7da1ad8fdf5a54444e34371c5877cb7a95ac
Archibald0 <dauvergne.fabien@gmail.com>
2017-07-24 13:10:13 +0200

    add renders to client controller

--- src/ChamaicaineBundle/Controller/ClientController.php
+++ src/ChamaicaineBundle/Controller/ClientController.php
@@ -2,6 +2,9 @@
 
 namespace ChamaicaineBundle\Controller;
 
+use ChamaicaineBundle\Entity\Date;
+use ChamaicaineBundle\Entity\DescEn;
+use ChamaicaineBundle\Entity\DescFr;
 use ChamaicaineBundle\Entity\Image;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -10,9 +13,15 @@ class ClientController extends Controller
     public function homeAction() {
         $em = $this->getDoctrine()->getManager();
         $images = $em->getRepository(Image::class)->findAll();
+        $descFr = $em->getRepository(DescFr::class)->findOneById(1);
+        $descEn = $em->getRepository(DescEn::class)->findOneById(1);
+        $dates = $em->getRepository(Date::class)->dateByDesc();
 
         return $this->render('@Chamaicaine/client/base_client.html.twig', array(
-            'images' => $images
+            'images' => $images,
+            'descFr' => $descFr,
+            'descEn' => $descEn,
+            'dates' => $dates
         ));
     }
 }
